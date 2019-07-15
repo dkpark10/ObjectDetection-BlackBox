65c195b7389c756ff9a95721750ab4f14f4b5501
dkpark10 <43857226+dkpark10@users.noreply.github.com>
2019-07-15 11:37:58 +0900

    Update 주말에무적권완성.cpp

--- 주말에무적권완성.cpp
+++ 주말에무적권완성.cpp
@@ -1,4 +1,3 @@
-#include <opencv2/opencv.hpp>
 #include <sstream>
 #include <iostream>
 #include <cstdio>
@@ -27,6 +26,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/vfs.h>
+#include <opencv2/opencv.hpp>
 #define BUFSIZE 1024
 #define DISK_MAX 0
 #define DISK_AVA 1
@@ -34,14 +34,17 @@
 #define BY_SECOND 1
 #define VID_TIME time_t
 #define FOL_TIME time_t
+#define DEBUG
 #define ll long long
 using namespace std;
 using namespace cv;
+
 queue<string> folder_list;
 queue<string> video_list;
 const char path[100] = "/home/nvidia/DKblackbox/";
+string base_path = "/home/dokyun/DKVScode/Blackbox";
 
-namespace patch 
+namespace patch
 {
 	template < typename T > std::string to_string(const T& n)
 	{
@@ -51,12 +54,12 @@ namespace patch
 	}
 }
 
-string Init_Pipeline(int width, int height, double fps)  // 뭔진 모르지만 젯슨에서 카메라 활성화 해야 할 때 인자로 넘겨줘야함
+string Init_Pipeline(int width, int height, double fps)
 {
-	return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" +\
-	       patch::to_string(width) + ", height=(int)" + patch::to_string(height)\
-	       + ",format=(string)NV12, framerate=(fraction)" + patch::to_string(fps) +\
-	       "/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw,\
+	return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + \
+		patch::to_string(width) + ", height=(int)" + patch::to_string(height)\
+		+ ",format=(string)NV12, framerate=(fraction)" + patch::to_string(fps) + \
+		"/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw,\
 	       format=(string)BGR ! appsink";
 }
 
@@ -171,91 +174,162 @@ extern void TimetoString(time_t timer, char *str, int time_type)
 
 string Video_Date_Name()
 {
-    return "/home/nvidia/DKblackbox/" + folder_list.front() + "/" +
+    return "/home/dokyun/DKVScode/Blackbox" + folder_list.front() + "/" +
            video_list.front() + ".avi";
-}
 
-bool Is_Changed_Time(char **prev, char **cur)
-{
-    if(**prev != **cur) return true;
-    else return false;
+    // return "/home/nvidia/DKblackbox" + folder_list.front() + "/" 
+    // + video_list.front() + ".avi";
 }
 
 int Create_Folder(time_t timer)
 {
-    char prev_time[100];
-    TimetoString(timer, prev_time, BY_SECOND);
-    video_list.push(prev_time);
-    TimetoString(timer, prev_time, BY_MINUTE);
-    folder_list.push(prev_time);
+    char fol[100];
+    char vid[100];
 
-    if (mkdir(prev_time, 0776) < 0 && errno != EEXIST){ //$$$$$$$$$$$$$
+    TimetoString(timer, fol, BY_MINUTE);
+    folder_list.push(fol);
+    
+    if (video_list.size() > 1){
+        video_list.pop();
+        TimetoString(timer, vid, BY_SECOND);
+        video_list.push(vid);
+    }
+    else{
+        TimetoString(timer, vid, BY_SECOND);
+        video_list.push(vid);
+    }
+
+    if (mkdir(fol, 0776) < 0 && errno != EEXIST){                //$$$$$$$$$$$$$
         perror("Create Folder Fail\n");
         return -1;
     }
+
     return 0;
+}
+
+int Remove_Dir(const char *path, int is_error_stop)
+{
+    DIR *dir_ptr = NULL;
+    struct dirent *file = NULL;
+    struct stat buf;
+    char filename[1024];
+
+    if ((dir_ptr = opendir(path)) == NULL){
+        return unlink(path);
+    }
+
+    while ((file = readdir(dir_ptr)) != NULL){
+
+        if (file->d_name[0] == '.')
+            continue;
+        
+        sprintf(filename, "%s/%s", path, file->d_name);
+
+        if (lstat(filename, &buf) == -1)
+            continue;
+
+        if (S_ISDIR(buf.st_mode)){
+            if (Remove_Dir(filename, is_error_stop) == -1 && is_error_stop){
+                return -1;
+            }
+        }
+        else if (S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode)){
+            if (unlink(filename) == -1 && is_error_stop)
+                return -1;
+        }
+    }
+    closedir(dir_ptr);
+    return rmdir(path);
 }
 
 int Video_Record()
 {
     Mat img;
 
+    ll max_size = Get_Disk_Size(DISK_MAX);
+    ll ava_size = Get_Disk_Size(DISK_AVA);
+    ll limit = max_size * 0.2;
+
     //비디오 캡쳐 초기화
-    VideoCapture cap(Init_Pipeline(640,480,30.0));
-    if (!cap.isOpened()){
-        cerr << "cap opened is fail"<< endl;
-        return -1;
-    }
+    VideoCapture cap(0);
+	if (!cap.isOpened()) {
+		cerr << "cap opened is fail" << endl;
+		return -1;
+	}
 
-    // 동영상 파일을 저장하기 위한 준비
-    Size size = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
-                     (int)cap.get(CAP_PROP_FRAME_HEIGHT));
+	// 동영상 파일을 저장하기 위한 준비
+	// Size size = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
+	// 	(int)cap.get(CAP_PROP_FRAME_HEIGHT));
 
-    VideoWriter writer;
-    int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+	Size size = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
+					 (int)cap.get(CAP_PROP_FRAME_HEIGHT));
 
-    FOL_TIME prev_fol = time(NULL);
-    ll folder_hour = (ll)prev_fol + 3600;
-    Create_Folder(prev_fol);
+	VideoWriter writer;
+	double fps = 30.0;
+	int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
 
-    /////////////////////// REC START!! ///////////////////
+    FOL_TIME prev_fol = time(NULL);                       // init time
+    Create_Folder(prev_fol);                              // init create
+
+    //////////////////////////////////////////////////////// REC START!! ///////////////////////////////////////////////////////////////////////////
     while (1){
-
 
         string video_date_name = Video_Date_Name();
 
-        VID_TIME begin_time = time(NULL);
-        ll video_minute = (ll)begin_time + 60;
-        writer.open(video_date_name, fourcc, 30.0, size, true);
+        FOL_TIME end_folder_time = time(NULL);
+        VID_TIME begin_vid_time = time(NULL);
+        
+        ll folder_hour = (ll)prev_fol + 3600;
+        ll video_minute = (ll)begin_vid_time + 60;
 
-        if (!writer.isOpened()){
-            cout << "error!! uu uu so sad " << endl;
-            return 1;
+        writer.open("123.avi", fourcc, 30.0, size, true);
+        time_t init = time(NULL);
+		ll minute = (ll)init + 60;
+        writer.open("123.avi", fourcc, 30.0, size, true);
+           
+        if (!writer.isOpened()) {
+			cout << "error!! uu uu so sad " << endl;
+			return 1;
+		}
+
+        if(end_folder_time == folder_hour){
+            
+            char new_folder_time_name[100];
+            Create_Folder(end_folder_time);
+            TimetoString(end_folder_time, new_folder_time_name, BY_MINUTE);
+            folder_list.push(new_folder_time_name);
+            prev_fol = (ll)end_folder_time;
         }
         
+        if(limit > ava_size){
+            char del_target[100]= "/home/dokyun/DKVScode/Blackbox/";
+            char temp[100] = "";
+
+            for(int i=0; i< folder_list.front().size(); i++){
+                temp[i] = folder_list.front().at(i);
+            }
+            strcat(del_target, temp);
+            Remove_Dir(del_target, 0);
+        }
+
         while (1){
-
+            
             VID_TIME end_video_time = time(NULL);
-            FOL_TIME end_folder_time = time(NULL);
-            char new_video_time_name[100];
+			time_t timer = time(NULL);
+          
             cap.read(img);
-
             if (img.empty()){
                 cerr << "empty video uu terrible >.<" << endl;
                 break;
             }
 
             writer.write(img);      //동영상 파일에 한 프레임을 저장함.
-
             imshow("Color", img);
             printf("cur disk size is %lld\n", Get_Disk_Size(DISK_AVA));
             waitKey(15);
 
-            // if(end_folder_time == folder_hour){
-
-            // }
-
             if (end_video_time == video_minute){
+                char new_video_time_name[100];
                 TimetoString(end_video_time, new_video_time_name, BY_SECOND);
                 video_list.pop();
                 video_list.push(new_video_time_name);
@@ -263,6 +337,7 @@ int Video_Record()
             }
         }
     }
+
     writer.release();
     cap.release();
 
@@ -282,174 +357,7 @@ int main(int argc, char *argv[])
     // dir = opendir(path);
 
     // Queue* dir_list = Create_DirQueue();
-    char prev_time[100];
-    char cur_time[100];
-    char *prev_folder_hour;
-    char *cur_folder_hour;
-    char *del_dir_name;
-    char *loot = (char *)malloc(sizeof(char) * 22);
-    strcpy(loot,"./");
 
-    time_t timer1 = time(NULL);                 // 현재 시각을 초 단위로 얻기
-
-    TimetoString(timer1, prev_time, BY_SECOND);
-    video_list.push(prev_time);
-    TimetoString(timer1, prev_time, BY_MINUTE);
-    folder_list.push(prev_time);
-    // Push_DirName(dir_list, prev_time);
-
-    prev_folder_hour = strstr(prev_time, "_");         // "_"의 주소 반환
-    prev_folder_hour += 2;                             // + 2만큼 포인터 연산해서 시간을 가리킴
-
-    if (mkdir(prev_time, 0776) < 0 && errno != EEXIST){    //$$$$$$$$$$$$$
-        perror("Create Folder Fail\n");
-        return -1;
-    }
     Video_Record();
-
-    // printf("%s",prev_time); // pass pass pass pass
-
-    while (1){
-        
-        ll max_size = Get_Disk_Size(DISK_MAX);
-        ll ava_size = Get_Disk_Size(DISK_AVA);
-        ll limit = max_size * 0.45;
-
-        // printf("prev_time is %s\n", prev_time);
-        time_t timer2 = time(NULL);
-        TimetoString(timer2, cur_time, BY_MINUTE);
-        // printf("cur_time is %s\n", cur_time);
-
-        cur_folder_hour = strstr(cur_time, "_");
-        cur_folder_hour += 2;
-
-        if (Is_Changed_Time(&prev_folder_hour, &cur_folder_hour)){
-            if (mkdir(cur_time, 0776) < 0 && errno != EEXIST){  //$$$$$$$$$$$$$
-                perror("Create Folder Fail\n");
-                return -1;
-            }
-            // Push_DirName(dir_list, cur_time);
-            folder_list.push(cur_time);
-            strcpy(prev_time, cur_time);
-        }
-        if(limit > ava_size){
-            // del_dir_name = Get_Del_DirName(dir_list);  //$$$$$$$$$$$$$
-            if(remove(strcat(loot, del_dir_name)) == -1){
-                perror("Remove file fail\n");
-                return -1;
-            }
-            if(remove(del_dir_name) == -1){
-                perror("Remove folder rail\n");
-                return -1;
-            }
-            // Pop_DirName(dir_list);
-            folder_list.pop();
-        }
-    }
-    // free(dir_list);
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <ctime>
-#include <time.h>
-#include <algorithm>
-#include <deque>
-#include <vector>
-#define ll long long
-using namespace cv;
-using namespace std;
-
-namespace patch
-{
-	template < typename T > std::string to_string(const T& n)
-	{
-		std::ostringstream stm;
-		stm << n;
-		return stm.str();
-	}
-}
-
-string Init_Pipeline(int width, int height, double fps) 
-{
-	return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" +\
-	       patch::to_string(width) + ", height=(int)" + patch::to_string(height)\
-	       + ",format=(string)NV12, framerate=(fraction)" + patch::to_string(fps) +\
-	       "/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw,\
-	       format=(string)BGR ! appsink";
-}
-
-int main()
-{
-    Mat img;
-    
-    deque<string> ds;
-    ds.push_back("123.avi");
-    ds.push_back("456.avi");
-    ds.push_back("789.avi");
-    ds.push_back("abc.avi");
-    ds.push_back("xyz.avi");
-
-    //비디오 캡쳐 초기화
-    VideoCapture cap(Init_Pipeline(640,480,30.0));
-    if (!cap.isOpened()){
-        cerr << "cap opened is fail"<< endl;;
-        return -1;
-    }
-
-    // 동영상 파일을 저장하기 위한 준비
-    Size size = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
-                     (int)cap.get(CAP_PROP_FRAME_HEIGHT));
-
-    VideoWriter writer;
-    double fps = 30.0;
-    int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
-
-    while (1){
-        
-        time_t init = time(NULL);
-        ll minute = (ll)init + 60;
-        writer.open(ds.front(), fourcc, fps, size, true);
-        
-        if (!writer.isOpened()){
-            cout << "error!! uu uu so sad " << endl;
-            return 1;
-        }
-        
-        while (1){
-            time_t timer = time(NULL);
-            cap.read(img);
-
-            if (img.empty()){
-                cerr << "empty video" << endl;
-                break;
-            }
-
-            //동영상 파일에 한 프레임을 저장함.
-            writer.write(img);
-
-            imshow("Color", img);
-            waitKey(15);
-            if (timer == minute)
-            {
-                ds.pop_front();
-                break;
-            }
-        }
-    }
-
     return 0;
 }
